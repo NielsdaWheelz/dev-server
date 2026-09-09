6f49c6a88a3966a9b563160c4c001bd28cef97dd
Niels-Erik Nandal <nnandal@Niels-Eriks-MacBook-Pro.local>
2026-09-09 16:02:29 -0700

    Align Skid release fixture with declared v0.2.30 pin

--- tests/skidbladnir.sh
+++ tests/skidbladnir.sh
@@ -262,11 +262,11 @@ test_pin_and_config_contract() (
 
   skidbladnir_release_pin_file="$pin"
   line="$(skidbladnir_release_values arch)" || fail 'current release pin was rejected'
-  assert_eq v0.2.28 "${line%%$'\t'*}" 'current release version'
-  [[ "$line" == *$'\thttps://github.com/NielsdaWheelz/skidbladnir/releases/download/v0.2.28/skidbladnir-linux-amd64.tar.gz\tc46da726970d0281dc62e3b9fb5ce28d2ab1d2caa01a849766754dcc1c95d595\tlinux-amd64' ]] ||
+  assert_eq v0.2.30 "${line%%$'\t'*}" 'current release version'
+  [[ "$line" == *$'\thttps://github.com/NielsdaWheelz/skidbladnir/releases/download/v0.2.30/skidbladnir-linux-amd64.tar.gz\t258a3ab35e89d345c3239f2fa2b457aa725217790c6e09eab29f73d0f724243e\tlinux-amd64' ]] ||
     fail 'current Linux URL or digest differs'
   cp "$pin" "$valid"
-  sed 's/"version": "v0.2.28"/"version": "v0.2.28", "version": "v0.2.28"/' \
+  sed 's/"version": "v0.2.30"/"version": "v0.2.30", "version": "v0.2.30"/' \
     "$valid" >"$duplicate"
   skidbladnir_release_pin_file="$duplicate"
   if skidbladnir_release_values arch >/dev/null 2>&1; then
